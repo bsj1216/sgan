c9a8fd2c77dfaead15a1d94bcd41df14ddd69a81
Sangjae Bae <sbae@sv870165lx.am.mds.honda.com>
2019-07-15 15:42:33 -0700

    fixed

--- sgan/data/loader.py
+++ sgan/data/loader.py
@@ -3,19 +3,18 @@ from torch.utils.data import DataLoader
 from sgan.data.trajectories import TrajectoryDataset, seq_collate
 
 
-def data_loader(args, data):
+def data_loader(args, path):
     dset = TrajectoryDataset(
-        data,
+        path,
         obs_len=args.obs_len,
         pred_len=args.pred_len,
         skip=args.skip,
         delim=args.delim)
 
     loader = DataLoader(
-       dset,
-       batch_size=args.batch_size,
-       shuffle=True,
-       num_workers=args.loader_num_workers,
-       collate_fn=seq_collate)
-    
+        dset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.loader_num_workers,
+        collate_fn=seq_collate)
     return dset, loader
